51a759522e62c9808731b4b5e0023c9caed9dc8e
Nikita Kovalyov <laqwoter@gmail.com>
2016-03-26 12:42:35 +0200

    passing tests on movement

--- rover.rb
+++ rover.rb
@@ -7,20 +7,43 @@ class Plato
 end
 
 class Rover
-  def initialize(plato, coordinates)
-  end
+  STATES = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+  CARDINAL_DIRECTION_TO_STATE = { 'N' => 0, 'E' => 1, 'S' => 2, "W" => 3 }
+  STATE_TO_CARDINAL_DIRECTION = CARDINAL_DIRECTION_TO_STATE.invert
+  MOVEMENT_STEPS = { 'L' => 3, 'R' => 1 }
 
-  def plato
+  def initialize(plato, coordinates)
+    @plato = plato
+    @x, @y, @direction = coordinates.split(" ")
+    @x = @x.to_i
+    @y = @y.to_i
+    @direction_number = CARDINAL_DIRECTION_TO_STATE[@direction]
   end
 
   def move(commands)
+    commands.split("").each do |command|
+      if command == "M"
+        do_move
+      else
+        rotate(command)
+      end
+    end
   end
 
   def current_location
+    "#{@x} #{@y} #{@direction}"
   end
 
   private
-  def rotate
+
+  def do_move
+    x, y = STATES[@direction_number]
+    @x += x
+    @y += y
   end
 
+  def rotate(command)
+    @direction_number = (@direction_number + MOVEMENT_STEPS[command]) % 4
+    @direction = STATE_TO_CARDINAL_DIRECTION[@direction_number]
+  end
 end
--- rover_spec.rb
+++ rover_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Rover do
   context 'moving on plato' do
     it 'can correctly move' do
       rover1.move("LMLMLMLMM")
-      rover2.move("LMLMLMLMM")
+      rover2.move("MMRMMRMRRM")
       expect(rover1.current_location).to eq("1 3 N")
       expect(rover2.current_location).to eq("5 1 E")
     end
