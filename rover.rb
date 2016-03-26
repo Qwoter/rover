f548e43b0f8a49da059ae688b4bc1e76cff6d3fe
Nikita Kovalyov <laqwoter@gmail.com>
2016-03-26 13:37:30 +0200

    refactoring: moved state machine to module, error handling placeholders

--- rover.rb
+++ rover.rb
@@ -1,3 +1,26 @@
+module RoverStateMachine
+  STATES = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+  CARDINAL_DIRECTION_TO_STATE = { 'N' => 0, 'E' => 1, 'S' => 2, "W" => 3 }
+  STATE_TO_CARDINAL_DIRECTION = CARDINAL_DIRECTION_TO_STATE.invert
+  MOVEMENT_STEPS = { 'L' => 3, 'R' => 1 }
+
+  def direction_to_i(direction)
+    CARDINAL_DIRECTION_TO_STATE[direction]
+  end
+
+  def coordinates_delta(direction)
+    STATES[direction]
+  end
+
+  def recalculate_direction(direction, command)
+    (direction + MOVEMENT_STEPS[command]) % 4
+  end
+
+  def direction_to_s(direction)
+    STATE_TO_CARDINAL_DIRECTION[@direction]
+  end
+end
+
 class Plato
   attr_reader :x, :y
 
@@ -7,43 +30,47 @@ class Plato
 end
 
 class Rover
-  STATES = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-  CARDINAL_DIRECTION_TO_STATE = { 'N' => 0, 'E' => 1, 'S' => 2, "W" => 3 }
-  STATE_TO_CARDINAL_DIRECTION = CARDINAL_DIRECTION_TO_STATE.invert
-  MOVEMENT_STEPS = { 'L' => 3, 'R' => 1 }
+  include RoverStateMachine
 
   def initialize(plato, coordinates)
     @plato = plato
-    @x, @y, @direction = coordinates.split(" ")
-    @x = @x.to_i
-    @y = @y.to_i
-    @direction_number = CARDINAL_DIRECTION_TO_STATE[@direction]
+    x, y, direction = coordinates.split(" ")
+    @x = x.to_i
+    @y = y.to_i
+    @direction = direction_to_i(direction)
   end
 
   def move(commands)
     commands.split("").each do |command|
-      if command == "M"
+      case command
+      when 'M'
         do_move
-      else
+      when 'L', 'R'
         rotate(command)
+      else
+        #TODO raise error on wrong command
       end
     end
   end
 
   def current_location
-    "#{@x} #{@y} #{@direction}"
+    "#{@x} #{@y} #{direction_name}"
   end
 
   private
 
   def do_move
-    x, y = STATES[@direction_number]
+    x, y = coordinates_delta(@direction)
     @x += x
     @y += y
+    #TODO check out of bounds
   end
 
   def rotate(command)
-    @direction_number = (@direction_number + MOVEMENT_STEPS[command]) % 4
-    @direction = STATE_TO_CARDINAL_DIRECTION[@direction_number]
+    @direction = recalculate_direction(@direction, command)
+  end
+
+  def direction_name
+    direction_to_s(@direction)
   end
 end
